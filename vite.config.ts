0aa14847d20aa7434daed28ff9c5449f937875fe
mariest <mesteberena@gmail.com>
2025-12-03 05:57:30 -0600

    arreglo en configuracion de dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,7 +8,10 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL)
-  }
-})
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(
+      process.env.VITE_API_BASE_URL || ''
+    ),
+  },
+});
+
 
